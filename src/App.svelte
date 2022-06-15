f50a61fb89726e9f0a5bcc682f6f5144753e3167
松本直樹 <nmatsumoto@matsumotonaokinoMacBook-Pro.local>
2022-06-15 17:35:18 +0900

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import LineCharts from "./components/LineCharts.svelte";
+</script>
+
+<main>
+  <h3>Line Charts</h3>
+  <LineCharts />
+</main>
+
+<style>
+  main {
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    height: 50%;
+  }
+
+  h3 {
+    text-align: center;
+  }
+</style>
---
+++ src/components/LineCharts.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Line from "svelte-chartjs/src/Line.svelte";
+
+  const options = {
+    responsive: true,
+    scales: {
+      yAxes: {
+        id: "y-axis-1",
+        type: "linear",
+        position: "right",
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  let data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(45,164,78)",
+        borderColor: "rgb(45,164,78)",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        yAxisID: "y-axis-1",
+      },
+      {
+        label: "My Second dataset",
+        backgroundColor: "rgb(207,34,46)",
+        borderColor: "rgb(207,34,46)",
+        data: [28, 48, 40, 19, 86, 27, 90],
+        yAxisID: "y-axis-1",
+      },
+    ],
+  };
+</script>
+
+<div>
+  <Line {data} {options} />
+</div>
